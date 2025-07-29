713a03c11f423cbd8c880d411dc7f687f9ae035e
Mark Lee <hasbuildpackbundlerupgradedyet@lazymalevolence.com>
2025-07-29 00:36:31 -0700

    refactor: replace werkzeug/gunicorn with starlette/uvicorn

--- python/server.py
+++ python/server.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Copyright (C) 2016 Mark Lee
+#!/usr/bin/env python3
+# Copyright (C) 2016, 2025 Mark Lee
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -14,107 +13,140 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Python HTTP server for "has buildpack Bundler upgraded yet".
+"""
 
-import json
-from lxml import etree
 import os
 import re
-from redis import StrictRedis
-import requests
-import semver
-from werkzeug.exceptions import MethodNotAllowed, NotAcceptable, NotFound
-from werkzeug.wrappers import Request, Response
+from pathlib import Path
 
-INDEX_HTML = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                          '..',
-                                          'index.html'))
-MIN_BUNDLER_VERSION = os.environ.get('MIN_BUNDLER_VERSION', '1.16.0')
+import requests
+from accept_types import get_best_match  # type: ignore[import-untyped]
+from lxml import etree
+from redis.asyncio import Redis
+from semver import Version
+from starlette.applications import Starlette
+from starlette.requests import Request
+from starlette.responses import HTMLResponse, JSONResponse, Response
+from starlette.routing import Route
+from starlette.status import HTTP_406_NOT_ACCEPTABLE
+
+INDEX_HTML = (Path(__file__) / ".." / ".." / "index.html").resolve()
+MIN_BUNDLER_VERSION = os.environ.get("MIN_BUNDLER_VERSION", "1.16.0")
 REGEX = re.compile(r'BUNDLER_VERSION += "(.+?)"')
-RUBY_LANGPACK_RELEASES_URL = '''\
+RUBY_LANGPACK_RELEASES_URL = """\
 https://github.com/heroku/heroku-buildpack-ruby/releases.atom\
-'''
-RUBY_LANGPACK_URL = '''\
+"""
+RUBY_LANGPACK_URL = """\
 https://raw.githubusercontent.com/heroku/heroku-buildpack-ruby/{}/\
 lib/language_pack/ruby.rb\
-'''
+"""
+TYPE_JSON = "application/json"
+TYPE_HTML = "text/html"
 
 
-def latest_buildpack_release(redis):
-    releases = requests.get(RUBY_LANGPACK_RELEASES_URL).text
-    xml = etree.fromstring(releases.encode('utf-8'))
-    nsmap = {'atom': xml.nsmap[None]}
-    release = xml.xpath('atom:entry/atom:id/text()', namespaces=nsmap)[0].rsplit('/', 1)[1]
-    if redis:
-        redis.setex('latest_buildpack_release', 3600, release)
-    return release
+class Buildpack:
+    """Represents a Ruby buildpack."""
+
+    redis: Redis | None
+    http: requests.Session
+
+    def __init__(self, session: requests.Session, redis: Redis | None) -> None:
+        """Creates a new Buildpack instance."""
+        self.redis = redis
+        self.http = session
+
+    async def latest_release(self) -> str:
+        """
+        Determines the latest tagged version of the Ruby buildpack, which is
+        cached in Redis if provided.
+        """
+        releases = self.http.get(RUBY_LANGPACK_RELEASES_URL).text
+        xml = etree.fromstring(releases.encode("utf-8"))
+        nsmap = {"atom": xml.nsmap[None]}
+        release: str = xml.xpath("atom:entry/atom:id/text()", namespaces=nsmap)[
+            0
+        ].rsplit(
+            "/",
+            1,
+        )[1]
+        if self.redis:
+            await self.redis.setex("latest_buildpack_release", 3600, release)
+        return release
+
+    async def bundler_version(self) -> Version | None:
+        """
+        Determines the Bundler version required for the latest buildpack
+        release, expressed as a semver Version.
+        """
+        buildpack_release: str | None = None
+        if self.redis:
+            buildpack_release = await self.redis.get("latest_buildpack_release")
+        if not buildpack_release:
+            buildpack_release = await self.latest_release()
+
+        ruby_langpack_url = RUBY_LANGPACK_URL.format(buildpack_release)
+        ruby_file = requests.get(ruby_langpack_url).text
+        match = REGEX.search(ruby_file)
+        if match:
+            bundler_version = match.group(1)
+            if self.redis:
+                await self.redis.hsetnx(
+                    "bundler_version",
+                    buildpack_release,
+                    bundler_version,
+                )  # type: ignore[misc]
+            return Version.parse(bundler_version)
+        return None
 
 
-def bundler_version_from_ruby_buildpack(redis):
-    buildpack_release = None
-    if redis:
-        buildpack_release = redis.get('latest_buildpack_release')
-    if not buildpack_release:
-        buildpack_release = latest_buildpack_release(redis)
-
-    ruby_langpack_url = RUBY_LANGPACK_URL.format(buildpack_release)
-    ruby_file = requests.get(ruby_langpack_url).text
-    match = REGEX.search(ruby_file)
-    if match:
-        bundler_version = match.group(1)
-        if redis:
-           redis.hsetnx('bundler_version', buildpack_release, bundler_version)
-        return bundler_version
-    else:
-       return None
-
-
-def is_bundler_upgraded(redis):
-    version_from_buildpack = bundler_version_from_ruby_buildpack(redis)
-    if version_from_buildpack is None:
+async def is_bundler_upgraded(session: requests.Session, redis: Redis | None) -> bool:
+    """
+    Determines if the version of Bundler used by the Ruby buildpack is greater
+    than the minimum version of Bundler.
+    """
+    buildpack = Buildpack(session, redis)
+    version = await buildpack.bundler_version()
+    if version is None:
         return False
-    match = '>={}'.format(MIN_BUNDLER_VERSION)
-    return semver.match(version_from_buildpack, match)
+    return version.match(f">={MIN_BUNDLER_VERSION}")
 
 
-def html_result(result):
+def html_result(result: bool) -> str:
+    """Render a boolean as emoji-fied HTML."""
     if result:
         return '<p class="yes"><i class="emoji"></i>Yes</p>'
-    else:
-        return '<p class="no"><i class="emoji"></i>No</p>'
+    return '<p class="no"><i class="emoji"></i>No</p>'
 
 
-def to_html(result):
+def to_html(result: bool) -> str:
+    """Render the result of ``is_bundler_upgraded`` as HTML."""
     try:
-        with open(INDEX_HTML) as f:
+        with INDEX_HTML.open() as f:
             index = f.read()
-        index = index.replace('{{ is_bundler_upgraded }}', html_result(result))
-        return index.replace('{{ MIN_BUNDLER_VERSION }}', MIN_BUNDLER_VERSION)
-    except IOError as e:
-        return 'HTML NOT FOUND: {}'.format(e)
+        index = index.replace("{{ is_bundler_upgraded }}", html_result(result))
+        return index.replace("{{ MIN_BUNDLER_VERSION }}", MIN_BUNDLER_VERSION)
+    except OSError as e:
+        return f"HTML NOT FOUND: {e}"
 
 
-def to_json(data):
-    return json.dumps({'result': data})
+async def upgraded(request: Request) -> Response:
+    """
+    HTTP handler that determines if the version of Bundler in the latest Ruby
+    buildpack is greater than or equal to the minimum Bundler version.
+    """
+    redis_url = os.environ.get("REDIS_URL")
+    redis = Redis.from_url(redis_url) if redis_url else None
+    with requests.Session() as session:
+        result = await is_bundler_upgraded(session, redis)
+
+    content_type = get_best_match(request.headers.get("accept"), [TYPE_JSON, TYPE_HTML])
+    if content_type == TYPE_JSON:
+        return JSONResponse({"result": result})
+    if content_type == TYPE_HTML:
+        return HTMLResponse(to_html(result))
+    return Response("JSON or HTML output only", status_code=HTTP_406_NOT_ACCEPTABLE)
 
 
-def application(environ, start_response):
-    request = Request(environ)
-    if request.method != 'GET':
-        raise MethodNotAllowed('GETs only')
-    if request.path != '/':
-        raise NotFound('Root URL only')
-    if request.accept_mimetypes.accept_json:
-        mime_type = 'application/json'
-        serialize = to_json
-    elif request.accept_mimetypes.accept_html:
-        mime_type = 'text/html'
-        serialize = to_html
-    else:
-        raise NotAcceptable('JSON or HTML output only')
-    redis_url = os.environ.get('REDIS_URL')
-    if redis_url:
-        redis = StrictRedis.from_url(redis_url)
-    else:
-        redis = None
-    response = Response(serialize(is_bundler_upgraded(redis)), mimetype=mime_type)
-    return response(environ, start_response)
+app = Starlette(routes=[Route("/", upgraded)])
